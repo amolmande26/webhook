4ceddaaf8acad1a16afb3ccfb54eaf94abe65d64
Amol <amolmande26@gmail.com>
2025-07-06 20:20:42 +0530

    Update SpringBootSimpleAppApplication.java

--- RestfulApp/src/main/java/com/example/app/SpringBootSimpleAppApplication.java
+++ RestfulApp/src/main/java/com/example/app/SpringBootSimpleAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSimpleAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSimpleAppApplication.class, args);
 	}
