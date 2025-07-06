6266c856f2af04fb06ab88087356aa75fdde05d9
Amol <amolmande26@gmail.com>
2025-07-06 13:58:21 +0530

    App updates

--- RestfulApp/src/main/java/com/example/app/controller/RestController.java
+++ RestfulApp/src/main/java/com/example/app/controller/RestController.java
@@ -1,7 +1,5 @@
 package com.example.app.controller;
 
-import com.example.app.service.GCPStorageBucketService;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,21 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/myApp")
 public class RestController {
 
-
-   private GCPStorageBucketService gcpStorageBucketService;
-
-   public RestController(GCPStorageBucketService gcpStorageBucketService) {
-        this.gcpStorageBucketService = gcpStorageBucketService;
-   }
-
     @GetMapping("/getValue")
     public String getValue() {
         return "It's success call";
-    }
-
-    @GetMapping("/getGSBucketData")
-    public String getGSBucketData() {
-        return gcpStorageBucketService.getGSBucketData();
     }
 
 }
